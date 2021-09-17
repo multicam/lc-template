556b5dc249293f6c565d20a8fb8ea26c63e1d5f6
Jean-Marc Giorgi <jmgiorgi@gmail.com>
2021-09-18 06:55:00 +1000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { LayerCake, Svg } from 'layercake';
+
+	import Line from './components/Line.svelte';
+	import Area from './components/Area.svelte';
+	import AxisX from './components/AxisX.svelte';
+	import AxisY from './components/AxisY.svelte';
+
+	import points from './data/points.csv';
+
+	const xKey = 'myX';
+	const yKey = 'myY';
+
+	points.forEach(row => {
+		row[yKey] = +row[yKey];
+	});
+</script>
+
+<style>
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
+
+<div class="chart-container">
+	<LayerCake
+		padding={{ right: 10, bottom: 20, left: 25 }}
+		x={xKey}
+		y={yKey}
+		yDomain={[0, null]}
+		data={points}
+	>
+		<Svg>
+			<AxisX/>
+			<AxisY/>
+			<Line/>
+			<Area/>
+		</Svg>
+	</LayerCake>
+</div>
